8a760163bea4109d9861e33624b86f2295c6be55
Geoff Schmit <gcschmit@gmail.com>
2022-10-04 11:49:23 -0500

    add linked list implementation

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -41,18 +41,29 @@ public class LinkedList
         Removes the first element in the linked list.
         @return the removed element
     */
+    public Object removeFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
 
-
-
-
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
     /**
         Adds an element to the front of the linked list.
         @param element the element to add
     */
-
-
-
+    public void addFirst(Object element)
+    {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
+    }
 
 
     /**
@@ -72,59 +83,126 @@ public class LinkedList
     }
 
 
-    class LinkedListIterator //implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
       //private data
-
+      private Node position;
+      private Node previous;
+      private boolean isAfterNext;
 
         /**
             Constructs an iterator that points to the front
             of the linked list.
         */
-
+        public LinkedListIterator()
+        {
+            position = null;
+            previous = null;
+            isAfterNext = false;
+        }
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
+        public Object next()
+        {
+            if(!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
 
+            previous = position;
+            isAfterNext = true;
 
+            if(position == null)
+            {
+                position = first;
+            }
+            else
+            {
+                position = position.next;
+            }
 
-
+            return position.data;
+        }
 
         /**
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
+        public boolean hasNext()
+        {
+            if(position == null)
+            {
+                return first != null;
+            }
 
+            return position.next != null;
+        }
 
         /**
             Adds an element before the iterator position
             and moves the iterator past the inserted element.
             @param element the element to add
         */
+        public void add(Object element)
+        {
+            if(position == null)
+            {
+                addFirst(element);
+                position = first;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next;
+                position.next = newNode;
+                position = newNode;
+            }
 
-
-
-
-
+            isAfterNext = false;
+        }
 
         /**
             Removes the last traversed element. This method may
             only be called after a call to the next() method.
         */
+        public void remove()
+        {
+            if(!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
 
+            if(position == first)
+            {
+                removeFirst();
+                position = null;
+            }
+            else
+            {
+                previous.next = position.next;
+                position = previous;
+            }
 
-
-
-
-
+            isAfterNext = false;
+        }
 
         /**
             Sets the last traversed element to a different value.
             @param element the element to set
         */
+        public void set(Object element)
+        {
+            if(!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
 
+            position.data = element;
+        }
 
 
 
